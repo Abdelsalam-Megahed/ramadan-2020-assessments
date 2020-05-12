27030e72be3b64ff58223ee99c7e7395b536cba8
abdelsalam.megahed <abdelsalam.megahed@gmail.com>
2020-05-12 19:25:21 +0300

    Adding and listing videoRequest functionality is done.
    Using pure JavaScript.

---
+++ client.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formVideoRequest = document.querySelector('#formVideoRequest');
+    const listOfVideoRequests = document.querySelector('#listOfRequests');
+   
+
+    fetch('http://localhost:7777/video-request')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((videoRequest) => {
+                // const video = getSingleVideoRequest(videoRequest);
+                listOfVideoRequests.appendChild(getSingleVideoRequest(videoRequest));
+            })
+        });
+
+    formVideoRequest.addEventListener('submit', function(event){
+      event.preventDefault();
+      const formData = new FormData(this);
+
+      fetch('http://localhost:7777/video-request', {
+          method: 'POST',
+          // body: JSON.stringify(formData) 
+          body: formData 
+      })
+      .then(function (response) {
+          return response.json();
+      })
+    })
+})
+
+function getSingleVideoRequest(videoRequest) {
+    const videoRequestContainer = document.createElement('div');
+    videoRequestContainer.innerHTML = `
+        <div class="card mb-3">
+        <div class="card-body d-flex justify-content-between flex-row">
+        <div class="d-flex flex-column">
+            <h3>${videoRequest.topic_title}</h3>
+            <p class="text-muted mb-2">${videoRequest.topic_details}</p>
+            <p class="mb-0 text-muted">
+            <strong>Expected results:</strong> ${videoRequest.expected_result} 
+            </p>
+        </div>
+        <div class="d-flex flex-column text-center">
+            <a class="btn btn-link">🔺</a>
+            <h3>0</h3>
+            <a class="btn btn-link">🔻</a>
+        </div>
+        </div>
+        <div class="card-footer d-flex flex-row justify-content-between">
+        <div>
+            <span class="text-info">${videoRequest.status.toUpperCase()}</span>
+            &bullet; added by <strong>${videoRequest.author_name}</strong> on
+            <strong>${new Date(videoRequest.submit_date).toLocaleDateString()}</strong>
+        </div>
+        <div
+            class="d-flex justify-content-center flex-column 408ml-auto mr-2"
+        >
+            <div class="badge badge-success">
+            ${videoRequest.target_level}
+            </div>
+        </div>
+        </div>
+        </div>
+    `;
+
+    return videoRequestContainer;
+}
+
+    
